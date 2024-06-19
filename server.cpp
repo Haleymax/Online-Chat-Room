3d26e25ea18a4c23617ee359dcd686be54e7efda
haley <huanghongweimax@163.com>
2024-06-19 15:23:42 +0800

    完成服务器主函数的编写

--- server.cpp
+++ server.cpp
@@ -36,7 +36,17 @@ public:
 
 private:
     //找出连接列表中为空位置的索引
-    int findEmptyIndex();
+    int findEmptyIndex(){
+        for (size_t i = 0; i < MAXMEN; i++)
+        {
+            if (connfds[i] == -1)
+            {
+                return i;
+            }
+            
+        }
+        return -1; //没有空位置
+    }
 };
 
 Server::Server(){
@@ -62,3 +72,45 @@ Server::Server(){
 
     listen(listenfd,MAXMEN);  //将套接字设置为监听状态并设置连接队列为MAXMEN
 }
+
+void Server::Start(){
+    cout << "服务器启动" << endl;
+    cout << "提示：输入命令“quit”以退出服务器" << endl;
+    thread quitThread(&Server::quit , this);   //退出线程
+    quitThread.detach();    //分离线程
+
+    connfds.resize(MAXMEN,-1); //初始化连接数组初始值为-1
+
+    while (true){
+        struct sockaddr_in cli_addr;   //客户端信息
+        socklen_t len = sizeof(cli_addr);
+        int connfd = accept(listenfd , (struct sockaddr *)&cli_addr,&len);  //与客户端建立连接
+        if (connfd < 0)
+        {
+            perror("fail to accept");
+            continue;   //连接失败继续读下一次的连接
+        }
+
+        char buff[BUFFSIZE];
+        time_t ticks = time(NULL);   //创建时间戳记录连接
+        cout << " 连接时间 : " << ctime(&ticks) 
+             << " 来自 : " << inet_ntop(AF_INET,&(cli_addr.sin_addr),buff,BUFFSIZE) 
+             << " 端口 : " << ntohs(cli_addr.sin_port)
+             << endl;
+
+        int index = findEmptyIndex();
+        if (index != -1)
+        {
+            connfds[index] = connfd;   //将套接字放入队列中
+            thread clientThread(&Server::rcv_snd , this , index);  //创建线程处理客户端连接
+            clientThread.detach();
+        }else {
+            cout << "连接已满，拒绝请求请稍后再试...." << endl;
+            close(connfd);
+        }
+        
+    }
+
+}
+
+
